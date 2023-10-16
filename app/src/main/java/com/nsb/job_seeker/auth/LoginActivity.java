6b4825e8bbd47f0fc9e08604e39e2b7dfc5a6299
Son3a <sonba7b1@gmail.com>
2023-10-16 15:25:44 +0700

    add google signin

--- app/src/main/java/com/nsb/job_seeker/auth/LoginActivity.java
+++ app/src/main/java/com/nsb/job_seeker/auth/LoginActivity.java
@@ -107,12 +107,9 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void setEvent() {
-
         clickLogin();
         clickRegister();
         clickForgotPassword();
-
-        //clickLoginGoogle();
     }
 
     private void initGoogle() {
@@ -126,7 +123,7 @@ public class LoginActivity extends AppCompatActivity {
                         // Your server's client ID, not your Android client ID.
                         .setServerClientId(getString(R.string.web_client_id))
                         // Only show accounts previously used to sign in.
-                        .setFilterByAuthorizedAccounts(true)
+                        .setFilterByAuthorizedAccounts(false)
                         .build())
                 // Automatically sign in when exactly one credential is retrieved.
                 .setAutoSelectEnabled(true)
